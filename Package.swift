df0f4956eac4a239ed93dbd0c7b70557e35129fd
runner <runner@Mac-1683095076784.local>
2023-05-03 06:31:46 +0000

    New walletsdk version 1.0.1-SNAPSHOT-d1f2800

--- Package.swift
+++ Package.swift
@@ -9,9 +9,9 @@ SPDX-License-Identifier: Apache-2.0
 
 import PackageDescription
 
-let version = "1.0.1-SNAPSHOT-ecb2033"
+let version = "1.0.1-SNAPSHOT-d1f2800"
 let moduleName = "walletsdk"
-let checksum = "e2f6ae88e47ff209613c0acb6300ac4a1d0690b76bd9454a9bdf77a3718a6769"
+let checksum = "44c1918de7378448679583360efea4158f5fdcd5a1c99c893f8ec51bff4575d9"
 
 let package = Package(
     name: moduleName,
